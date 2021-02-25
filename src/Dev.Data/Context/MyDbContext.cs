a6ebbe39003c38c419c4e530725af8839df797f7
Felipe <dev03.nuvem3@outlook.com>
2021-02-25 11:58:43 -0300

    Initial Commit

---
+++ src/Dev.Data/Context/MyDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Dev.Business.Models;
+using System.Linq;
+
+namespace Dev.Data.Context
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions options): base(options) { }
+
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {            
+
+            //Inserção dos Mappings do banco
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(MyDbContext).Assembly);
+
+            //Desativação da operação Cascade do banco
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e=>e.GetForeignKeys()))
+            {
+                relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+            }
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
